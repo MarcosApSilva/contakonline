5f58108bc3d268dc103625cb0d6a8334b92252fe
MarcosApSilva <103291480+MarcosApSilva@users.noreply.github.com>
2025-01-31 07:42:12 -0400

    Atualização da pagina

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
         <div className="content">
           <h1 className="display-4 text-white">Bem-vindo ao site da Contak</h1><br /><br />
           <div className="button-container">
-            <a href="https://contakonline.netlify.app" className="btn btn-primary btn-lg">Contak Contabilidade</a>
-            <a href="https://www.exemplo2.com" className="btn btn-success btn-lg">Chatbot</a>
-            <a href="https://www.exemplo2.com" className="btn btn-danger btn-lg">Fiscal</a>
+            <a href="https://contak.net.br" className="btn btn-primary btn-lg">Contak Contabilidade</a>
+            <a href="https://contakbot.net.br" className="btn btn-success btn-lg">Chatbot</a>
+            <a href="https://contakfiscal.net.br" className="btn btn-danger btn-lg">Fiscal</a>
           </div>
         </div>
       </div>
